ece88d54fbf3780cfef5062bbd2146a89e308e02
jhwrwe <jalexander05@student.ciputra.ac.id>
2023-11-12 12:38:08 +0700

    aa

--- app/Models/Teachers.php
+++ app/Models/Teachers.php
@@ -10,7 +10,7 @@ class Teachers extends Model
 
     use HasFactory;
 
-    protected $fillable = ['name','majors','work_experience','attended','skills','email','password','username','nationality','gender','Additional_information','Hobbys','age','image'];
+    protected $fillable = ['name','majors','work_experience','attended','skills','email','password','username','nationality','gender','Additional_information','Hobbys','age','image','about','description','phone'];
     public static function index()
     {
         return self::all();
