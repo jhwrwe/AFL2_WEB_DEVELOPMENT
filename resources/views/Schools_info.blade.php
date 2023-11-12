96824b8e3cc58d80a2d6c652becd2a9c5c27c8db
jhwrwe <jalexander05@student.ciputra.ac.id>
2023-11-12 13:30:31 +0700

    w

--- resources/views/Schools_info.blade.php
+++ resources/views/Schools_info.blade.php
@@ -1,3 +1,21 @@
 @extends('layouts.template')
 @section('layout_content')
+<body class="bg-gray-100 font-sans">
+    <div class="container mx-auto py-12">
+        <div class="bg-white shadow-md rounded-lg p-8">
+            <div class="grid grid-cols-1 sm:grid-cols-2 gap-8">
+                <div class="text-center">
+                    <img src="/images/{{ $project['image'] }}" alt="{{ $project['name'] }}" class="w-full h-auto rounded-lg shadow-md">
+                </div>
+                <div>
+                    <h1 class="text-3xl font-bold mb-4">{{ $project['name'] }}</h1>
+                    <p class="text-gray-600 mb-4 text-lg"><span class="font-semibold">Course:</span> {{ $project['course'] }}</p>
+                    <p class="text-gray-600 mb-4 text-lg"><span class="font-semibold">criteria:</span> {{ $project['criteria'] }}</p>
+                    <p class="text-gray-600 mb-4 text-lg"><span class="font-semibold">About Course:</span> {{ $project['aboutcourse'] }}</p>
+                    <a href="#" class="block text-center bg-blue-500 text-white rounded-lg py-3 hover:bg-blue-600 transition duration-300">Apply Now</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
 @endsection
--- resources/views/view_schools.blade.php
+++ resources/views/view_schools.blade.php
@@ -1,26 +1,26 @@
-
 @extends('layouts.template')
 @section('layout_content')
 
 <body class="bg-gray-100 font-sans">
     <div class="container mx-auto py-12" >
         <h1 class="text-4xl font-bold text-center mb-8">Meet Our School Representatives</h1>
-        <ul class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8">
+        <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
             @foreach($projects as $pro)
             <a href="/Schools_info/{{$pro['id']}}">
-                <li class="bg-white shadow-md rounded-lg overflow-hidden">
-
-                    <img src="images\{{ $pro['image'] }}" alt="images\{{ $pro['image'] }}" class="w-full h-64 object-cover object-center">
-                    <div class="p-6">
-                        <h2 class="text-xl font-bold mb-2">name: {{ $pro['name'] }}</h2>
-                        <p class="text-gray-600 mb-4">course: {{ $pro['course'] }}</p>
-                        <p class="text-gray-600 mb-4">Major: {{ $pro['criteria'] }}</p>
-                        <a href="/Schools_info/{{$pro['id']}}" class="block text-center bg-blue-500 text-white rounded-lg py-2 hover:bg-blue-600 transition duration-300">Learn More</a>
+                <div class="w-full sm:w-auto">
+                    <div class="bg-white shadow-md rounded-lg overflow-hidden mb-4">
+                        <img src="images/{{ $pro['image'] }}" alt="images/{{ $pro['image'] }}" class="w-full h-48 object-cover object-center">
+                        <div class="p-6">
+                            <h2 class="text-xl font-bold mb-2">{{ $pro['name'] }}</h2>
+                            <p class="text-gray-600 mb-2">Course: {{ $pro['course'] }}</p>
+                            <p class="text-gray-600 mb-2">Major: {{ $pro['criteria'] }}</p>
+                            <a href="/Schools_info/{{$pro['id']}}" class="block text-center bg-blue-500 text-white rounded-lg py-2 hover:bg-blue-600 transition duration-300">Learn More</a>
+                        </div>
                     </div>
-                </li>
+                </div>
             </a>
             @endforeach
-        </ul>
+        </div>
     </div>
 </body>
 @endsection
