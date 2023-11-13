55a4ae8f396aff73ae3294ebe88c009bd7a27957
jhwrwe <jalexander05@student.ciputra.ac.id>
2023-11-13 09:32:03 +0700

    ss

--- resources/views/view_apply.blade.php
+++ resources/views/view_apply.blade.php
@@ -35,6 +35,7 @@
 <br />
 <br />
 <br />
+<br />
 
 <div class="container mx-auto px-2 pt-4 pb-12 text-gray-800">
     <h2 class="w-full my-2 text-5xl font-bold leading-tight text-center text-gray-800">
